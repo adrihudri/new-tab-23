75d0baa173767a498f2ba8ec16ab03381540f49f
Adriana Lachká <lachka.adriana@gmail.com>
2024-12-10 21:55:47 +0100

    update: 1.01

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,282 +1,293 @@
+let isEditMode = false;
 
-// List all icons from the "brands" folder
-var iconFiles = [
-  "google",
-  "facebook",
-  "twitter",
-  "amazon",
-  "apple",
-  "facebook",
-  "twitter",
-  "amazon",
-  "apple",
-  "facebook",
-  "twitter",
-  "amazon",
-  "apple",
-  "facebook",
-  "twitter",
-  "amazon",
-  "apple",
-  "facebook",
-  "twitter",
-  "amazon",
-  "apple",
-  "android"
-  // Add more icon filenames as needed
-];
+document.getElementById('gear-icon').addEventListener('click', toggleEditMode);
+document.getElementById('apply-button').addEventListener('click', applyChanges);
+document.getElementById('widgets-width-input').addEventListener('input', adjustWidthAndValue);
+document.getElementById('time-format-input').addEventListener('change', setTimeAndDate);
 
-// Toggle accordion panel visibility
-function toggleAccordion() {
-  this.classList.toggle("active");
-  var panel = this.nextElementSibling;
-  panel.classList.toggle("show");
-}
-
-// Attach event listeners to accordion buttons
-var accordionButtons = document.querySelectorAll(".accordion-btn");
-accordionButtons.forEach(function (button) {
-  button.addEventListener("click", toggleAccordion);
+document.addEventListener('DOMContentLoaded', function() {
+  loadSettings();
+  setDynamicWelcomeMessage();
+  setTimeAndDate();
+  setInterval(setTimeAndDate, 1000); // Update the time and date every second
 });
 
-// Populate the icon dropdown with the available icons
-var iconSelect = document.getElementById("iconSelect");
-var dropdownContainer = document.getElementById("dropdownContainer");
-var selectedIconInput = document.getElementById("selectedIcon");
-
-iconFiles.forEach(function (filename) {
-  var iconPath = `assets/brands/${filename}.svg`;
-
-  var optionElement = document.createElement("div");
-  optionElement.classList.add("icon-option");
-  optionElement.innerHTML = `<img src="${iconPath}">`;
-
-  optionElement.addEventListener("click", function () {
-    selectedIconInput.style.backgroundImage = `url(${iconPath})`;
-    selectedIconInput.dataset.iconPath = iconPath; // Store the icon path as a data attribute
-    selectedIconInput.value = filename; // Display the filename in the input field
-    closeDropdown();
-  });
-
-  dropdownContainer.appendChild(optionElement);
-});
-
-// Add an icon
-function addIcon() {
-  var iconValue = selectedIconInput.dataset.iconPath;
-  var urlValue = document.getElementById("url").value;
-
-  if (!iconValue || !urlValue.trim()) {
-    alert("Please select an icon and provide a URL.");
-    return;
-  }
-
-  savedIcons.push({ icon: iconValue, url: urlValue });
-
-  // Clear the input fields
-  document.getElementById("url").value = "";
-  selectedIconInput.style.backgroundImage = "none";
-  selectedIconInput.dataset.iconPath = "";
-  selectedIconInput.value = "";
-
-  // Refresh the displayed icons
-  displaySavedIcons();
+function toggleEditMode() {
+  isEditMode = !isEditMode;
+  document.getElementById('edit-mode-container').classList.toggle('hidden');
+  if (isEditMode) loadEditMode();
 }
 
+function loadSettings() {
+  chrome.storage.sync.get(['name', 'widgetsWidth', 'is24HourFormat', 'city', 'backgroundKeyword'], function(items) {
+    if (items.name) document.getElementById('name').textContent = items.name;
+    if (items.widgetsWidth) document.getElementById('container').style.width = items.widgetsWidth + 'px';
+    if (items.is24HourFormat != null) document.getElementById('time-format-input').checked = items.is24HourFormat;
+    if (items.city) document.getElementById('city-input').value = items.city;
+    setWeather(); // Set weather after loading the city
+    setTimeAndDate(); // Set time and date after loading the time format
 
-
-// Open the dropdown
-function openDropdown() {
-  dropdownContainer.classList.add("show");
-}
-
-// Close the dropdown
-function closeDropdown() {
-  dropdownContainer.classList.remove("show");
-}
-
-// Attach event listeners to show/hide the dropdown
-iconSelect.addEventListener("click", openDropdown);
-document.addEventListener("click", function (event) {
-  if (!iconSelect.contains(event.target) && !dropdownContainer.contains(event.target)) {
-    closeDropdown();
-  }
-});
-
-
-
-// Initialize savedIcons with an empty array
-var savedIcons = [];
-
-
-// Remove an icon
-function removeIcon(index) {
-  savedIcons.splice(index, 1);
-  displaySavedIcons();
-}
-
-// Edit the URL of an icon
-function editIcon(index) {
-  var newURL = prompt("Enter the new URL for the icon:", savedIcons[index].url);
-  if (newURL !== null) {
-    savedIcons[index].url = newURL;
-    displaySavedIcons();
-  }
-}
-
-// Display the saved icons
-function displaySavedIcons() {
-  var savedIconsContainer = document.getElementById("savedIcons");
-  savedIconsContainer.innerHTML = "";
-
-  savedIcons.forEach(function (savedIcon, index) {
-    var iconContainer = document.createElement("div");
-    iconContainer.classList.add("saved-icon");
-
-    var iconLink = document.createElement("a");
-    iconLink.href = savedIcon.url;
-    iconLink.target = "_blank";
-
-    var iconImage = document.createElement("img");
-    iconImage.src = savedIcon.icon;
-
-    iconLink.appendChild(iconImage);
-    iconContainer.appendChild(iconLink);
-
-    var editButton = document.createElement("button");
-    editButton.classList.add("edit-button");
-    editButton.innerHTML = '<img class="icon-btns" src="assets/img/edit.svg">';
-    editButton.addEventListener("click", function () {
-      editIcon(index);
-    });
-    iconContainer.appendChild(editButton);
-
-    var removeButton = document.createElement("button");
-    removeButton.classList.add("remove-button");
-    removeButton.innerHTML = '<img class="icon-btns" src="assets/img/remove.svg">';
-    removeButton.addEventListener("click", function () {
-      removeIcon(index);
-    });
-    iconContainer.appendChild(removeButton);
-
-    savedIconsContainer.appendChild(iconContainer);
+    // Use the saved background keyword or default to 'Nature'
+    const backgroundKeyword = items.backgroundKeyword || 'Nature';
+    setBackgroundImage(backgroundKeyword);
   });
 }
 
 
+function loadEditMode() {
+  chrome.storage.sync.get(['name', 'widgetsWidth', 'is24HourFormat', 'city', 'backgroundKeyword'], function(items) {
+    document.getElementById('name-input').value = items.name || '';
+    const widgetsWidth = items.widgetsWidth || 1000;
+    document.getElementById('widgets-width-input').value = widgetsWidth;
+    document.getElementById('widgets-width-value').textContent = widgetsWidth + 'px';
+    document.getElementById('time-format-input').checked = items.is24HourFormat || false;
+    document.getElementById('city-input').value = items.city || 'London';
 
-function displaySavedIconsContent() {
-  var savedIconsContainer = document.getElementById("icons");
-  savedIconsContainer.innerHTML = "";
+    // Background settings
+    const backgroundSelect = document.getElementById('background-select');
+    const customKeywordInput = document.getElementById('custom-keyword-input');
+    const customKeywordContainer = document.getElementById('custom-keyword-container');
 
-  savedIcons.forEach(function (savedIcon, index) {
-    var iconContainer = document.createElement("div");
-    iconContainer.classList.add("saved-icon");
+    // **Declare 'savedKeyword' before using it**
+    const savedKeyword = items.backgroundKeyword || 'Nature';
 
-    var iconLink = document.createElement("a");
-    iconLink.href = savedIcon.url;
-    iconLink.target = "_blank";
+    // **Declare 'predefinedCategories' before using it**
+    const predefinedCategories = ['Nature', 'Cities', 'Animals', 'Space', 'Christmas'];
 
-    var iconImage = document.createElement("img");
-    iconImage.src = savedIcon.icon;
-
-    iconLink.appendChild(iconImage);
-    iconContainer.appendChild(iconLink);
-
-    savedIconsContainer.appendChild(iconContainer);
-  });
-}
-
-// Open the settings panel
-document.getElementById("openBtn").addEventListener("click", function () {
-  document.getElementById("settingsPanel").style.width = "250px";
-  document.getElementById("settingsPanel").style.padding = "60px 20px 20px 20px";
-});
-
-// Close the settings panel
-document.getElementById("closeBtn").addEventListener("click", function () {
-  document.getElementById("settingsPanel").style.width = "0";
-  document.getElementById("settingsPanel").style.padding = "0px";
-});
-
-// Save the settings
-document.getElementById("saveBtn").addEventListener("click", function () {
-  let name = document.getElementById("name").value;
-  chrome.storage.local.set({ name: name, icons: savedIcons }, function () {
-    alert("Settings saved.");
-    location.reload();
-  });
-});
-
-// Load the settings
-chrome.storage.local.get(["name", "icons"], function (data) {
-  document.getElementById("name").value = data.name || "";
-  if (Array.isArray(data.icons)) {
-    savedIcons = data.icons;
-  }
-  displaySavedIcons();
-  displaySavedIconsContent();
-  document.getElementById("greeting").innerText = getGreeting() + ", " + data.name;
-});
-
-// Get the greeting based on the time
-function getGreeting() {
-  let hour = new Date().getHours();
-  if (hour < 12) {
-    return "Good Morning";
-  } else if (hour < 18) {
-    return "Hello";
-  } else {
-    return "Good Evening";
-  }
-}
-
-// Attach event listener to add icon button
-document.getElementById("addIcon").addEventListener("click", addIcon);
-
-
-
-/* Update background randomly
-var backgrounds = ['image1.jpg', 'image2.jpg', 'image3.jpg'];
-document.body.style.backgroundImage = 'url(' + backgrounds[Math.floor(Math.random() * backgrounds.length)] + ')';
-*/
-
-// Display time and date
-setInterval(() => {
-  let date = new Date();
-  document.getElementById('time').innerText = date.toLocaleTimeString();
-  document.getElementById('date').innerText = date.toLocaleDateString();
-}, 1000);
-
-// Function to save the selected font to Chrome storage
-function saveFontToStorage(font) {
-  chrome.storage.local.set({ selectedFont: font }, function () {
-    console.log("Font saved to storage.");
-  });
-}
-
-// Function to retrieve the selected font from Chrome storage
-function getFontFromStorage(callback) {
-  chrome.storage.local.get("selectedFont", function (result) {
-    var selectedFont = result.selectedFont;
-    callback(selectedFont);
-  });
-}
-
-// Change font
-var fontSelect = document.getElementById("fontSelect");
-fontSelect.addEventListener("change", function () {
-  var selectedFont = this.value;
-  document.body.style.fontFamily = selectedFont;
-  saveFontToStorage(selectedFont);
-});
-
-// Read the selected font from storage on load
-document.addEventListener("DOMContentLoaded", function () {
-  getFontFromStorage(function (selectedFont) {
-    if (selectedFont) {
-      fontSelect.value = selectedFont;
-      document.body.style.fontFamily = selectedFont;
+    // Now use 'savedKeyword' and 'predefinedCategories'
+    if (predefinedCategories.includes(savedKeyword)) {
+      backgroundSelect.value = savedKeyword;
+      customKeywordContainer.style.display = 'none';
+    } else {
+      backgroundSelect.value = 'Custom';
+      customKeywordContainer.style.display = 'block';
+      customKeywordInput.value = savedKeyword;
     }
   });
-});
+}
+
+
+function adjustWidthAndValue() {
+  const newWidth = this.value;
+  document.getElementById('widgets-width-value').textContent = newWidth + 'px';
+  document.getElementById('container').style.width = newWidth + 'px';
+}
+
+function applyChanges() {
+  console.log('applyChanges is called');
+
+  const newName = document.getElementById('name-input').value;
+  const newWidgetsWidth = document.getElementById('widgets-width-input').value;
+  const is24HourFormat = document.getElementById('time-format-input').checked;
+  const newCity = document.getElementById('city-input').value;
+  const newUnit = document.getElementById('unit-input').value;
+
+  // Background settings
+  const backgroundSelectValue = document.getElementById('background-select').value;
+  let backgroundKeyword = backgroundSelectValue;
+  if (backgroundSelectValue === 'Custom') {
+    backgroundKeyword = document.getElementById('custom-keyword-input').value || 'Nature';
+  }
+
+  const nameElement = document.getElementById('name');
+  if (nameElement) {
+    nameElement.textContent = newName;
+  } else {
+    console.error('Element with ID "name" is not found in the document.');
+  }
+
+  document.getElementById('container').style.width = newWidgetsWidth + 'px';
+
+  chrome.storage.sync.set({
+    name: newName,
+    widgetsWidth: newWidgetsWidth,
+    is24HourFormat: is24HourFormat,
+    city: newCity,
+    unit: newUnit,
+    backgroundKeyword: backgroundKeyword
+  }, function() {
+    setTimeAndDate();
+    setWeather();
+    setBackgroundImage(backgroundKeyword);
+    toggleEditMode();
+  });
+}
+
+
+function setDynamicWelcomeMessage() {
+  chrome.storage.sync.get(['name'], function(items) {
+    const userName = items.name || 'User';
+    const hour = new Date().getHours();
+    let greeting;
+
+    if (hour >= 5 && hour < 12) greeting = "Good Morning";
+    else if (hour >= 12 && hour < 18) greeting = "Good Afternoon";
+    else if (hour >= 18 && hour < 22) greeting = "Good Evening";
+    else greeting = "Good Night";
+
+    const specialGreetings = [
+      "Rise and Shine",
+      "Time to Make the Donuts",
+      "Carpe Diem",
+      "Looking great",
+      "Time to Seize the Day",
+      "Let's Roll",
+      "Let's Make Today a Great One",
+      "Keep Pushing Forward",
+      "Your Day Awaits",
+      "Embrace the Challenge",
+      "You've Got This",
+      "Conquer the Day",
+      "Brighten Up!",
+      "The Future Is Yours",
+      "Make It Count",
+      "Awake and Achieve",
+      "Your Time to Shine",
+      "Onward and Upward",
+      "Focus and Flourish",
+      "Ignite Your Passion",
+      "Today's a Gift",
+      "Seize Every Moment",
+      "Champion Your Goals",
+      "Dare to Dream Big",
+      "Fuel Your Ambition",
+      "Start Strong",
+      "Embrace Your Potential",
+      "Opportunities Await",
+      "Your Journey Begins",
+      "Unleash Your Power",
+      "Strive for Greatness",
+      "Make It Happen",
+      "Design Your Destiny",
+      "Savor the Possibilities",
+      "Innovate and Inspire",
+      "Luminaries Lead On",
+      "Greatness Beckons",
+      "Overcome and Excel",
+      "Embrace the New Day",
+      "Celebrate Your Strengths",
+      "Push Your Boundaries",
+      "Rise Beyond Limits",
+      "Chase Your Vision",
+      "Your Path Is Clear",
+      "Unlock Your Potential",
+      "Radiate Confidence",
+      "Energize and Excel",
+      "The Day Is Yours",
+      "Shine Your Light",
+      "Fuel the Fire Within",
+      "Turn Hope into Action",
+      "Nurture Your Aspirations",
+      "Master Your Mindset",
+      "Welcome to Possibility",
+      "Embrace the Adventure",
+      "Set Your Intentions",
+      "Blaze a New Trail",
+      "Achieve the Extraordinary",
+      "Climb to New Heights",
+      "Focus, Flow, Flourish",
+      "Celebrate Each Step",
+      "Break New Ground",
+      "Smile and Succeed",
+      "Today Is for You",
+      "Let Your Spark Ignite"
+    ];
+    
+
+    if (Math.random() < 0.5) greeting = specialGreetings[Math.floor(Math.random() * specialGreetings.length)];
+
+    document.getElementById('welcome').textContent = greeting + ', ' + userName;
+  });
+}
+
+function setTimeAndDate() {
+  const now = new Date();
+  const is24HourFormat = document.getElementById('time-format-input').checked;
+  const timeOptions = { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: !is24HourFormat };
+  const dateOptions = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
+
+  document.getElementById('current-time').textContent = now.toLocaleTimeString('en-US', timeOptions);
+  document.getElementById('current-date').textContent = now.toLocaleDateString('en-US', dateOptions);
+}
+
+function setWeather() {
+  const apiKey = '5ffcc3768c02c3d860d1dae86b51a93e';
+  
+  chrome.storage.sync.get(['city', 'unit'], function(items) {
+    const city = items.city || 'London'; // Use stored city or default to London
+    const units = items.unit || 'metric'; // Use stored unit or default to 'metric'
+    
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&units=${units}&appid=${apiKey}`;
+
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        const description = data.weather[0].description;
+        const temperature = Math.round(data.main.temp);
+        const icon = data.weather[0].icon;
+        const unitSymbol = units === 'metric' ? '°C' : '°F';
+
+        document.getElementById('weather-city').textContent = data.name;
+        document.getElementById('weather-description').textContent = description;
+        document.getElementById('weather-temperature').textContent = `${temperature}${unitSymbol}`;
+        document.getElementById('weather-icon').src = `https://openweathermap.org/img/wn/${icon}.png`;
+      })
+      .catch(error => console.error('Error fetching the weather data:', error));
+  });
+}
+
+async function setBackgroundImage(keyword) {
+    const apiKey = 'EuPTXNpHrLXxzMl5Dra5oYPPFYfAFjVrxL1khW5gte2ZAUXqGtISzCfL'; // Replace with your Pexels API key
+    try {
+      const response = await fetch(`https://api.pexels.com/v1/search?query=${encodeURIComponent(keyword)}&per_page=80`, {
+        headers: {
+          Authorization: apiKey
+        }
+      });
+      const data = await response.json();
+  
+      if (data.photos && data.photos.length > 0) {
+        // Filter images with width >= 3840 (4K resolution)
+        const images = data.photos.filter(photo => photo.width >= 3840);
+  
+        if (images.length > 0) {
+          const randomImage = images[Math.floor(Math.random() * images.length)];
+          // Use the highest resolution available
+          const imageUrl = randomImage.src.original;
+  
+          document.body.style.backgroundImage = `url(${imageUrl})`;
+
+        } else {
+          console.error('No 4K images found for this keyword.');
+        }
+      } else {
+        console.error('No images found for this keyword.');
+      }
+    } catch (error) {
+      console.error('Error fetching image:', error);
+    }
+
+
+
+    
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const backgroundSelect = document.getElementById('background-select');
+    if (backgroundSelect) {
+      backgroundSelect.addEventListener('change', function () {
+        const customKeywordContainer = document.getElementById('custom-keyword-container');
+        if (this.value === 'Custom') {
+          customKeywordContainer.style.display = 'block';
+        } else {
+          customKeywordContainer.style.display = 'none';
+        }
+      });
+    }
+  });
+  
